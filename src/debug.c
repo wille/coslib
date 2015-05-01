71323ac3394e6dfb65ceb44e84176c965ddf0ea6
redpois0n <red@cmail.nu>
2015-05-01 14:23:04 +0200

    removed experiment

--- src/debug.c
+++ src/debug.c
@@ -1,15 +1,3 @@
-#include<stdio.h>
-
-extern char **environ;
-
 int main() {
-  int i = 1;
-  char *s = *environ;
-
-  for (; s; i++) {
-    printf("%s\n", s);
-    s = *(environ+i);
-  }
-
-  return 0;
+	return 0;
 }
